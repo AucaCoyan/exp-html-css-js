94e6a1ee38a1638e59662a9fad727336de967423
Auca Coyan <aucacoyan@gmail.com>
2025-04-15 19:59:10 -0300

    :sparkles: otel with deno

---
+++ debugging_with_deno/index.ts
@@ -0,0 +1,28 @@
+/// Open Telemetry in Deno
+/// Based on https://www.youtube.com/watch?v=u8FnYa31iek
+
+import { delay } from "jsr:@std/async";
+import { Hono } from "npm:hono";
+
+async function createUser(): Promise<string> {
+    const id = crypto.randomUUID();
+
+    console.log("Creating user with ID: ", id);
+
+    await delay(100); // simulate database
+
+    if (id[0] === "a") {
+        throw new Error("User Creation failed");
+    }
+    return id;
+}
+
+console.log(createUser());
+const app = new Hono();
+
+app.get("/", async (c) => {
+    const id = await createUser();
+    return c.json({ id });
+});
+
+Deno.serve(app.fetch);
